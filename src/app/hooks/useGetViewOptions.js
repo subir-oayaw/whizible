4fa46ff188187118242d1d38da61f446d2e9b5d8
subir-oayaw <139326457+subir-oayaw@users.noreply.github.com>
2024-08-02 16:07:27 +0530

    fix

---
+++ src/app/hooks/useGetViewOptions.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useLocation } from "react-router-dom";
+const useGetViewOptions = (tagid) => {
+  const [getViewOptions, setGetViewOptions] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const userdata = JSON.parse(sessionStorage.getItem("user"));
+  const roleID = userdata?.postId;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const accessToken = sessionStorage.getItem("access_token");
+        const response = await axios.get(
+          process.env.REACT_APP_BASEURL_ACCESS_CONTROL1 +
+            `/api/PageLevelAcess?tagId=${tagid}&roleID=${roleID}`,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`
+            }
+            // Optionally, if you need to ignore SSL certificate errors (not recommended in production):
+          }
+        );
+
+        if (response.status !== 200) {
+          throw new Error("Failed to fetch dashboard data");
+        }
+        setGetViewOptions(response.data.data.lstPageLevelAcess);
+        console.log("first44", response.data.data.lstPageLevelAcess);
+      } catch (error) {
+        setError(error.message);
+        console.error("Error fetching dashboard data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { getViewOptions, loading, error };
+};
+
+export default useGetViewOptions;
