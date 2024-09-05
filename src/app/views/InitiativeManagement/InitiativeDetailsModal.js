58e79137f07a000aab2ee2f93b35c5b60109a49d
subir-oayaw <139326457+subir-oayaw@users.noreply.github.com>
2024-09-05 09:58:44 +0530

    fix

--- src/app/views/InitiativeManagement/InitiativeDetailsModal.js
+++ src/app/views/InitiativeManagement/InitiativeDetailsModal.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Modal, Box, Typography, IconButton } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
-import TableProgressBar from "../../utils/TableProgressBar"; // Import the new component
 import "./InitiativeDetailsModal.css";
-import CustomProgressBar from "../../utils/CustomProgressBar";
+import GetInitiativeStageDetails from "../../hooks/GetInitiativeStageDetails/GetInitiativeStageDetails";
+
 const style = {
   position: "absolute",
   top: "50%",
@@ -18,8 +18,23 @@ const style = {
   overflowY: "auto" // Enable vertical scrolling
 };
 
-const InitiativeDetailsModal = ({ open, handleClose, initiative, stagesLegend }) => {
-  const { stagesCompleted, totalStages, stages, currentStage } = initiative;
+const InitiativeDetailsModal = ({ open, handleClose, initiative }) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await GetInitiativeStageDetails(initiative?.ideaIdPk);
+        setData(result?.data?.initiativeStageDetailsEntity[0]); // Getting the first entity from the response
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    if (initiative?.ideaIdPk) {
+      fetchData();
+    }
+  }, [initiative?.ideaIdPk]);
 
   return (
     <Modal
@@ -47,19 +62,23 @@ const InitiativeDetailsModal = ({ open, handleClose, initiative, stagesLegend })
                       <tbody>
                         <tr>
                           <td>Initiative title&nbsp;:</td>
-                          <td className="font-weight-600">{initiative?.title}</td>
+                          <td className="font-weight-600">{data?.title}</td>
                         </tr>
                         <tr>
                           <td>Initiated on&nbsp;:</td>
-                          <td className="font-weight-600">{initiative?.date}</td>
+                          <td className="font-weight-600">
+                            {data?.createdDate
+                              ? new Date(data?.createdDate).toLocaleDateString()
+                              : "N/A"}
+                          </td>
                         </tr>
                         <tr>
                           <td>Business group&nbsp;:</td>
-                          <td className="font-weight-600">{initiative?.businessGroup}</td>
+                          <td className="font-weight-600">{data?.businessGroup || "N/A"}</td>
                         </tr>
                         <tr>
-                          <td>Delivery unit&nbsp;:</td>
-                          <td className="font-weight-600">{initiative?.deliveryUnit}</td>
+                          <td>Organization unit&nbsp;:</td>
+                          <td className="font-weight-600">{data?.organizationunit || "N/A"}</td>
                         </tr>
                       </tbody>
                     </table>
@@ -70,86 +89,25 @@ const InitiativeDetailsModal = ({ open, handleClose, initiative, stagesLegend })
                       <tbody>
                         <tr>
                           <td>Initiative code&nbsp;:</td>
-                          <td className="font-weight-600">{initiative?.code}</td>
+                          <td className="font-weight-600">{data?.initiativeCode || "N/A"}</td>
                         </tr>
                         <tr>
-                          <td>Initiative category&nbsp;:</td>
-                          <td className="font-weight-600">{initiative?.category}</td>
+                          <td>Nature of demand&nbsp;:</td>
+                          <td className="font-weight-600">{data?.natureofDemand || "N/A"}</td>
                         </tr>
                         <tr>
-                          <td>Organization unit&nbsp;:</td>
-                          <td className="font-weight-600">{initiative?.organizationUnit}</td>
+                          <td>Delivery unit&nbsp;:</td>
+                          <td className="font-weight-600">{data?.deliveryUnit || "N/A"}</td>
                         </tr>
                         <tr>
                           <td>Delivery team&nbsp;:</td>
-                          <td className="font-weight-600">{initiative?.deliveryTeam}</td>
+                          <td className="font-weight-600">{data?.deliveryTeam || "N/A"}</td>
                         </tr>
                       </tbody>
                     </table>
                   </div>
                 </div>
               </div>
-            </div>
-            {/* <Box display="flex" alignItems="center" p={2} border={1} borderColor="grey.300" mb={2}>
-              <Typography variant="h6" component="h3" style={{ marginRight: "16px" }}>
-                Legend:
-              </Typography>
-              <div className="stagesLegendContainer">{stagesLegend}</div>
-            </Box> */}
-
-            {/* <Box p={2} border={1} borderColor="grey.300" mb={2}>
-              <div className="left-side">
-                <Typography variant="body2" className="due-in" color="textSecondary">
-                  {initiative.dueIn} Days
-                </Typography>
-              </div>
-              <CustomProgressBar
-                stagesCompleted={stagesCompleted}
-                totalStages={totalStages}
-                stages={stages}
-                initiative={initiative}
-              />
-              <Typography variant="body2" color="textSecondary">
-                {stagesCompleted} stages completed
-              </Typography>
-            </Box> */}
-            <div id="Project_Grid_panel" className="table-responsive stageGridtable m-3">
-              <table
-                className="table table-striped table-hover table-bordered init_borderedTbl"
-                id="stage-table"
-              >
-                <thead>
-                  <tr className="cart-table-head">
-                    {/* <th style={{ width: "50px" }}>Progress</th> */}
-                    <th>From Stage</th>
-                    <th>To Stage</th>
-                    <th>Action Taken</th>
-                    <th>Approver</th>
-                    <th>Completed on</th>
-                    <th>Comments</th>
-                    <th>Delayed by</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {stages?.map((stage, index) => (
-                    <tr key={index}>
-                      {/* <td>
-                        <TableProgressBar
-                          stageIndex={index} // Pass the current stage index
-                          currentStage={stagesCompleted} // Pass the current stage
-                        />
-                      </td> */}
-                      <td>{stage.from}</td>
-                      <td>{stage.to}</td>
-                      <td>{stage.actionTaken}</td>
-                      <td>{stage.approver}</td>
-                      <td>{stage.completedOn}</td>
-                      <td>{stage.comments}</td>
-                      <td>{stage.delayedBy}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
             </div>
           </div>
         </div>
