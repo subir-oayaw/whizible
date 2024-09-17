0d07f7a6bc60c0efae1133f2ead4f278234c616e
subir-oayaw <139326457+subir-oayaw@users.noreply.github.com>
2024-09-17 13:54:18 +0530

    fix

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -4,6 +4,7 @@ import "@fluentui/react/dist/css/fabric.css";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { WhizTheme } from "./components";
+import fetchUserProfile from "../app/hooks/fetchUserProfile";
 // ALL CONTEXTS
 import { AuthProvider } from "./contexts/JWTAuthContext";
 import SettingsProvider from "./contexts/SettingsContext";
@@ -13,20 +14,31 @@ import routes from "./routes";
 import "../fake-db";
 
 import "./app.css";
+
 export default function App() {
   const navigate = useNavigate();
   const content = useRoutes(routes);
+
   useEffect(() => {
-    const handleMessage = (event) => {
+    const handleMessage = async (event) => {
       if (event.data.action === "tokenReceived") {
         const accessToken = localStorage.getItem("access_token");
 
         if (accessToken) {
           sessionStorage.setItem("access_token", accessToken);
-        }
 
-        // Redirect the user
-        navigate("/landingPage");
+          try {
+            const user = await fetchUserProfile(accessToken);
+            console.log("user", JSON.stringify(user));
+            sessionStorage.setItem("user", JSON.stringify(user));
+          } catch (error) {
+            console.error("Failed to fetch user profile", error);
+            // Optionally handle the error, e.g., show an error message
+          }
+
+          // Redirect the user after user profile is fetched
+          navigate("/landingPage");
+        }
       }
     };
 
